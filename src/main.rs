2bc69a552ab0423c4461cc782582ae9fd5c7ba0a
Prince Bett <princedaryl3@tutanota.com>
2022-05-07 14:43:32 +0300

    refactor(App): add signup

--- src/main.rs
+++ src/main.rs
@@ -16,16 +16,26 @@ fn main() {
 #[component]
 fn App<G: Html>(cx: Scope) -> View<G> {
     view! { cx,
-        div(class="palette-01 frow  row-center height-100vhmin") {
-            div(class="frow col-md-1-2") {
-                div(class="svg-50 "){
+        div(class="frow  row-center h-100vh") {
+            div(class="frow centered-column col-md-1-3 bg-p minh-100vh") {
+                h1(class="fg-w") {
+                    "Mini Bank"
+                }
+                div(class="w-50"){
                     (svg_logo(cx))
                 }
             }
-            div(class="frow col-md-1-2") {
-                div(class="frow direction-column row-center width-100") {
-                    h3 { "Hello, Rusty MiniBank here!" }
-                    h4 { "Make by Rust-Nairobi Developers for Rusteceans" }
+            div(class="frow row-center col-md-2-3") {
+                SignUp()
+                // div(class="frow direction-column row-center width-100") {
+                //     h3 { "Hello, Rusty MiniBank here!" }
+                //     h4 { "Make by Rust-Nairobi Developers for Rusteceans" }
+                // }
+            }
+        }
+    }
+}
+
 #[component]
 fn SignUp<G: Html>(cx: Scope) -> View<G> {
     view! { cx,
